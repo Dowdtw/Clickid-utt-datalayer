4df3a957158ef39844398f4ea1e85aa5229d4a81
Dowdtw <47797395+Dowdtw@users.noreply.github.com>
2020-01-27 15:04:54 +0000

    Update datalayer.js

--- datalayer.js
+++ datalayer.js
@@ -1,3 +1,11 @@
-dataLayer = [];
+
+	dataLayer=[{
+	'customerId': 'customerId',
+ 	'customerEmail': '871876c13fecaf15b4d51df0b2b187dc7c1d8bb7',
+	'category': 'shoes',
+	'sku': 'black',
+	'amount': 100.00, 
+	}];
+	
 
 console.log(dataLayer);
